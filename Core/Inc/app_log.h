f17dac0da2641ea44280061914949fcea9a42e88
Ildefonso Aspera <ildefonso.aspera@gmail.com>
2020-11-15 14:47:35 +0100

    Improved format of log files and added optimized log function for when only a string is to be printed

--- Core/Inc/app_log.h
+++ Core/Inc/app_log.h
@@ -10,22 +10,29 @@
 
 #include "stdint.h"
 
-#define LOG_LEVEL_THRESHOLD		LOG_LVL_DEBUG
+/********* USER CONFIGURABLE DEFINES **********/
+#define LOG_LEVEL_THRESHOLD		LOG_LVL_DEBUG	// Lowest priority that will be accepted. Must be an item from enum log_lvl
 #define LOG_USE_ANSI_COLORS		1
-#define LOG_USE_RUNTIME			1
+#define LOG_USE_TIMESTAMP		1
+#define LOG_INTERNAL_BUFFER		100				// Limit to how long a string can be. Minimum 36 chars when using timestamps and colors
+/******* END USER CONFIGURABLE DEFINES ********/
+
 
 enum log_lvl{LOG_LVL_DEBUG, LOG_LVL_INFO, LOG_LVL_NOTIF, LOG_LVL_WARNING, LOG_LVL_ERROR};
 
-typedef uint32_t (*log_time_callback)(void);		// Returned value is expected to be ms
-typedef void (*log_out_callback)(char* str);		// Callback that will print the formatted string
+typedef uint32_t (*log_time_callback)(void);	// Callback to get the number of milliseconds since start
+typedef void (*log_out_callback)(char* str);	// Callback that will print the formatted string
 
-#define log_debug(...)		log_log(LOG_LVL_DEBUG,   __VA_ARGS__)
-#define log_info(...)		log_log(LOG_LVL_INFO,    __VA_ARGS__)
-#define log_notif(...)		log_log(LOG_LVL_NOTIF,   __VA_ARGS__)
-#define log_warn(...)		log_log(LOG_LVL_WARNING, __VA_ARGS__)
-#define log_error(...)		log_log(LOG_LVL_ERROR,   __VA_ARGS__)
+// Redirects to optimized function if called with only a string
+// Limits function call to maximum 6 arguments, but should be good enough
+#define _LOG_GET_MACRO(_1,_2,_3,_4,_5,_6,NAME,...) NAME
+#define log_debug(...) _LOG_GET_MACRO(__VA_ARGS__, log_log, log_log, log_log, log_log, log_log, log_string)(LOG_LVL_DEBUG, __VA_ARGS__)
+#define log_notif(...) _LOG_GET_MACRO(__VA_ARGS__, log_log, log_log, log_log, log_log, log_log, log_string)(LOG_LVL_NOTIF, __VA_ARGS__)
+#define log_warn(...)  _LOG_GET_MACRO(__VA_ARGS__, log_log, log_log, log_log, log_log, log_log, log_string)(LOG_LVL_WARNING, __VA_ARGS__)
+#define log_error(...) _LOG_GET_MACRO(__VA_ARGS__, log_log, log_log, log_log, log_log, log_log, log_string)(LOG_LVL_ERROR, __VA_ARGS__)
 
+void log_string(enum log_lvl lvl, const char* fmt);
 void log_log(enum log_lvl lvl, const char* fmt, ...);
-void log_init();
+void log_init(log_time_callback time_callback, log_out_callback out_callback);
 
 #endif /* INC_APP_LOG_H_ */
--- Core/Src/app_log.c
+++ Core/Src/app_log.c
@@ -12,47 +12,89 @@
 #include <inttypes.h>
 #include <string.h>
 #include "app_log.h"
+#include "main.h"
 
-#define LOG_INTERNAL_BUFFER		128		// Limit to how long a string can be
-#define LOG_DEFAULT_COLOR		"\x1b[0m"
+
+#if(LOG_USE_ANSI_COLORS)
+#define LOG_DEFAULT_COLOR		"\e[0m"
+#define LOG_BRIGHT_WHITE		"\e[97m"
+static const char lvl_colors[6][8] = {"\e[94m", "\e[36m", "\e[32m", "\e[33m", "\e[31m", "\e[35m"};
+#endif
 
 static const char lvl_letters[5][7] = {"DEBUG ", "INFO  ", "NOTIF ", "WARN  ", "ERROR "};
-static const char lvl_colors[6][8] = {"\x1b[94m", "\x1b[36m", "\x1b[32m", "\x1b[33m", "\x1b[31m", "\x1b[35m"};
 
 static log_time_callback m_time_callback = NULL;
 static log_out_callback  m_out_callback  = NULL;
 
 
-void log_log(enum log_lvl lvl, const char* fmt, ...)
+// This function assumes that the output buffer will be long enough
+static void print_header(enum log_lvl lvl, char* output)
 {
-	static char m_buffer[LOG_INTERNAL_BUFFER];
+#if(LOG_USE_TIMESTAMP)
+	if(m_time_callback != NULL)
+	{
+		uint32_t ms = m_time_callback();
+		uint32_t h = ms/3600000;
+		uint32_t m = (ms%3600000)/60000;
+		uint32_t s = (ms%60000)/1000;
+		ms = ms%1000;
+		sprintf(output, "%02lu:%02lu:%02lu.%03lu ", h, m, s, ms);
+	}
+#endif
+
+#if(LOG_USE_ANSI_COLORS)
+    strcat(output, lvl_colors[lvl]);
+#endif
+    strcat(output, lvl_letters[lvl]);
+#if(LOG_USE_ANSI_COLORS)
+    strcat(output, LOG_BRIGHT_WHITE);
+#endif
+
+}
+
+
+// Testing shows that log_string is 10% faster than log_log,
+// but further optimizations will be performed in the future
+void log_string(enum log_lvl lvl, const char* fmt)
+{
+	char m_buffer[LOG_INTERNAL_BUFFER];
+	int endingElems = sizeof("\r\n") + strlen(LOG_DEFAULT_COLOR)*LOG_USE_ANSI_COLORS;
 
 	if(m_out_callback != NULL && lvl >= LOG_LEVEL_THRESHOLD)
 	{
 		m_buffer[0] = '\0';
-#if(LOG_USE_RUNTIME)
-		if(m_time_callback != NULL)
-		{
-			uint32_t ms = m_time_callback();
-			uint32_t h = ms/3600000;
-			uint32_t m = (ms%3600000)/60000;
-			uint32_t s = (ms%60000)/1000;
-			ms = ms%1000;
-			snprintf(m_buffer, sizeof(m_buffer), "%02lu:%02lu:%02lu.%03lu ", h, m, s, ms);
-		}
+		print_header(lvl, m_buffer);
+
+		int currentLength = strlen(m_buffer);
+		strncat(m_buffer, fmt, LOG_INTERNAL_BUFFER - currentLength - endingElems);
+#if LOG_USE_ANSI_COLORS
+		strcat(m_buffer, LOG_DEFAULT_COLOR);
 #endif
-#if(LOG_USE_ANSI_COLORS)
-		strcat(m_buffer, lvl_colors[lvl]);
-		strcat(m_buffer, lvl_letters[lvl]);
-		strcat(m_buffer, "\x1b[97m");			// Bright white
-#endif
+		strcat(m_buffer, "\r\n");
+		m_out_callback(m_buffer);
+	}
+}
+
+
+void log_log(enum log_lvl lvl, const char* fmt, ...)
+{
+	char m_buffer[LOG_INTERNAL_BUFFER];
+	int endingElems = sizeof("\r\n") + strlen(LOG_DEFAULT_COLOR)*LOG_USE_ANSI_COLORS;
+
+	if(m_out_callback != NULL && lvl >= LOG_LEVEL_THRESHOLD)
+	{
+		m_buffer[0] = '\0';
+		print_header(lvl, m_buffer);
 
 		va_list va;
 		va_start(va, fmt);
-		vsnprintf(strchr(m_buffer, '\0'), LOG_INTERNAL_BUFFER - strlen(m_buffer) - 7, fmt, va);	// Account for elements
+		vsnprintf(strchr(m_buffer, '\0'), LOG_INTERNAL_BUFFER - strlen(m_buffer) - endingElems, fmt, va);	// Account for ending elements
 		va_end(va);
 
-		strcat(m_buffer, "\x1b[0m\r\n");
+#if LOG_USE_ANSI_COLORS
+		strcat(m_buffer, LOG_DEFAULT_COLOR);
+#endif
+		strcat(m_buffer, "\r\n");
 		m_out_callback(m_buffer);
 	}
 }
